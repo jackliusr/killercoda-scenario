7dbd9f0cad83b1cfca42248465535da4c48c20b2
Jack Liu Shurui <jackliusr@gmail.com>
2024-01-31 20:41:20 +0800

    Update verify.sh

--- k8s-container-lifecycle/verify.sh
+++ k8s-container-lifecycle/verify.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/bash
-if [[ $(kubectl logs nginx-lifecycle | grep -E -q "from (postStart|preStop) nginx-lifecycle") ]]
-then 
-  #not found
-  exit 1; 
-else
-  #found
-  exit 0;
-fi
+kubectl logs nginx-lifecycle | grep -E -q "from (postStart|preStop) nginx-lifecycle"
